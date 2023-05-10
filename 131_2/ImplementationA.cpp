0b52fed7ff6c76804a5f58b90ab18cbcff422682
Andy Tran <tranandy098@gmail.com>
2023-05-10 15:10:35 -0700

    static done, with pgm output 1,3,5

---
+++ 131_2/ImplementationA.cpp
@@ -0,0 +1,192 @@
+#include <omp.h>
+#include <algorithm>
+#include <cstdlib>
+#include <cctype>
+#include <cmath>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <vector>
+ 
+// g++ ImplementationA.cpp -o ImplementationA -Wall -std=c++11 -fopenmp -fPIE -pie -g
+// ./ImplementationA test1.pgm test1_output.pgm 25 static
+
+/* Global variables, Look at their usage in main() */
+int image_height;
+int image_width;
+int image_maxShades;
+int inputImage[3000][2000]; //should be dynamic for diffrent image sizes
+int outputImage[3000][2000]; //should be dynamic for diffrent image sizes
+int numChunks;
+
+/* ****************Change and add functions below ***************** */
+void compute_prewitt_static()
+{
+/* 3x3 Prewitt mask for X Dimension. */
+int maskX[3][3];
+maskX[0][0] = +1; maskX[0][1] = 0; maskX[0][2] = -1;
+maskX[1][0] = +1; maskX[1][1] = 0; maskX[1][2] = -1;
+maskX[2][0] = +1; maskX[2][1] = 0; maskX[2][2] = -1;
+
+/* 3x3 Prewitt mask for Y Dimension. */
+int maskY[3][3];
+maskY[0][0] = +1; maskY[0][1] = +1; maskY[0][2] = +1;
+maskY[1][0] =   0; maskY[1][1] =   0; maskY[1][2] =    0;
+maskY[2][0] =  -1; maskY[2][1] =  -1; maskY[2][2] =  -1;
+
+int x, y, i, j, grad_x, grad_y, grad;
+
+#pragma omp parallel private(x,y,i,j, grad_x, grad_y, grad) shared(inputImage, outputImage, image_height, image_width, maskX, maskY) num_threads(8)
+{
+    #pragma omp for schedule(static, image_height/numChunks)
+    for(x = 0; x < image_height; ++x ){
+        for(y = 0; y < image_width;  ++y ){
+            grad_x = 0;
+            grad_y = 0;
+            grad = 0;
+            /* For handling image boundaries */
+            if( x == 0 || x == (image_height-1) || y == 0 || y == (image_width-1))
+                grad = 0;
+            else{
+                /* Gradient calculation in X Dimension */
+                for(i = -1; i <= 1; i++ )  {
+                    for(j = -1; j <= 1; j++ ){
+                        grad_x += (inputImage[x+i][y+j] * maskX[i+1][j+1]);
+                    }
+                }
+                /* Gradient calculation in Y Dimension */
+                for(i=-1; i<=1; i++)  {
+                    for(j=-1; j<=1; j++){
+                        grad_y += (inputImage[x+i][y+j] * maskY[i+1][j+1]);
+                    }
+                }
+                /* Gradient magnitude */
+                grad = (int) sqrt( (grad_x * grad_x) + (grad_y * grad_y) );
+            }
+            if (grad < 0) grad = 0;
+            if (grad > 255) grad = 255;
+            #pragma omp critical 
+            {
+                outputImage[x][y] = grad;
+            }
+        }  
+    }
+}
+}
+
+void compute_prewitt_dynamic()
+{
+
+}
+/* **************** Change the function below if you need to ***************** */
+
+int main(int argc, char* argv[])
+{
+    if(argc != 5)
+    {
+        std::cout << "ERROR: Incorrect number of arguments. Format is: <Input image filename> <Output image filename> <# of chunks> <static/dynamic>" << std::endl;
+        return 0;
+    }
+ 
+    std::ifstream file(argv[1]);
+    if(!file.is_open())
+    {
+        std::cout << "ERROR: Could not open file " << argv[1] << std::endl;
+        return 0;
+    }
+    numChunks  = std::atoi(argv[3]);
+
+    std::cout << "Detect edges in " << argv[1] << " using OpenMP threads\n" << std::endl;
+
+    /* ******Reading image into 2-D array below******** */
+
+    std::string workString;
+    /* Remove comments '#' and check image format */ 
+    while(std::getline(file,workString))
+    {
+        if( workString.at(0) != '#' ){
+            if( workString.at(1) != '2' ){
+                std::cout << "Input image is not a valid PGM image" << std::endl;
+                return 0;
+            } else {
+                break;
+            }       
+        } else {
+            continue;
+        }
+    }
+    /* Check image size */ 
+    while(std::getline(file,workString))
+    {
+        if( workString.at(0) != '#' ){
+            std::stringstream stream(workString);
+            int n;
+            stream >> n;
+            image_width = n;
+            stream >> n;
+            image_height = n;
+            break;
+        } else {
+            continue;
+        }
+    }
+
+    /* Check image max shades */ 
+    while(std::getline(file,workString))
+    {
+        if( workString.at(0) != '#' ){
+            std::stringstream stream(workString);
+            stream >> image_maxShades;
+            break;
+        } else {
+            continue;
+        }
+    }
+    /* Fill input image matrix */ 
+    int pixel_val;
+    for( int i = 0; i < image_height; i++ )
+    {
+        if( std::getline(file,workString) && workString.at(0) != '#' ){
+            std::stringstream stream(workString);
+            for( int j = 0; j < image_width; j++ ){
+                if( !stream )
+                    break;
+                stream >> pixel_val;
+                inputImage[i][j] = pixel_val;
+            }
+        } else {
+            continue;
+        }
+    }
+
+    /************ Call functions to process image *********/
+    std::string opt = argv[4];
+    if( !opt.compare("static") )
+    {    
+        double dtime_static = omp_get_wtime();
+        compute_prewitt_static();
+        dtime_static = omp_get_wtime() - dtime_static;
+    } else {
+        double dtime_dyn = omp_get_wtime();
+        compute_prewitt_dynamic();
+        dtime_dyn = omp_get_wtime() - dtime_dyn;
+    }
+
+    /* ********Start writing output to your file************ */
+    std::ofstream ofile(argv[2]);
+    if( ofile.is_open() )
+    {
+        ofile << "P2" << "\n" << image_width << " " << image_height << "\n" << image_maxShades << "\n";
+        for( int i = 0; i < image_height; i++ )
+        {
+            for( int j = 0; j < image_width; j++ ){
+                ofile << outputImage[i][j] << " ";
+            }
+            ofile << "\n";
+        }
+    } else {
+        std::cout << "ERROR: Could not open output file " << argv[2] << std::endl;
+        return 0;
+    }
+    return 0;
+}
