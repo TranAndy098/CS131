00b85877b3727e68133f4334c10e1de376cd3117
Andy PC <trana@DESKTOP-TGR6CNB.(none)>
2023-05-10 22:33:19 -0700

    dynamic

--- 131_2/ImplementationA.cpp
+++ 131_2/ImplementationA.cpp
@@ -7,6 +7,7 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <map>
  
 // g++ ImplementationA.cpp -o ImplementationA -Wall -std=c++11 -fopenmp -fPIE -pie -g
 // ./ImplementationA test1.pgm test1_output.pgm 25 static
@@ -18,6 +19,8 @@ int image_maxShades;
 int inputImage[3000][2000]; //should be dynamic for diffrent image sizes
 int outputImage[3000][2000]; //should be dynamic for diffrent image sizes
 int numChunks;
+
+std::map<int, std::vector<int>> thread_starts;
 
 /* ****************Change and add functions below ***************** */
 void compute_prewitt_static()
@@ -34,29 +37,33 @@ maskY[0][0] = +1; maskY[0][1] = +1; maskY[0][2] = +1;
 maskY[1][0] =   0; maskY[1][1] =   0; maskY[1][2] =    0;
 maskY[2][0] =  -1; maskY[2][1] =  -1; maskY[2][2] =  -1;
 
-int x, y, i, j, grad_x, grad_y, grad;
+int x, y;
+int chunk_size = image_height/numChunks;
 
-#pragma omp parallel private(x,y,i,j, grad_x, grad_y, grad) shared(inputImage, outputImage, image_height, image_width, maskX, maskY) num_threads(8)
+#pragma omp parallel private(x) shared(inputImage, outputImage, image_height, image_width, thread_starts) num_threads(8)
 {
-    #pragma omp for schedule(static, image_height/numChunks)
+    #pragma omp for schedule(static, chunk_size) private(y)
     for(x = 0; x < image_height; ++x ){
+        if (x%chunk_size == 0) {
+            thread_starts[omp_get_thread_num()].push_back(x);
+        }
         for(y = 0; y < image_width;  ++y ){
-            grad_x = 0;
-            grad_y = 0;
-            grad = 0;
+            int grad_x = 0;
+            int grad_y = 0;
+            int grad = 0;
             /* For handling image boundaries */
             if( x == 0 || x == (image_height-1) || y == 0 || y == (image_width-1))
                 grad = 0;
             else{
                 /* Gradient calculation in X Dimension */
-                for(i = -1; i <= 1; i++ )  {
-                    for(j = -1; j <= 1; j++ ){
+                for(int i = -1; i <= 1; i++ )  {
+                    for(int j = -1; j <= 1; j++ ){
                         grad_x += (inputImage[x+i][y+j] * maskX[i+1][j+1]);
                     }
                 }
                 /* Gradient calculation in Y Dimension */
-                for(i=-1; i<=1; i++)  {
-                    for(j=-1; j<=1; j++){
+                for(int i=-1; i<=1; i++)  {
+                    for(int j=-1; j<=1; j++){
                         grad_y += (inputImage[x+i][y+j] * maskY[i+1][j+1]);
                     }
                 }
@@ -69,14 +76,68 @@ int x, y, i, j, grad_x, grad_y, grad;
             {
                 outputImage[x][y] = grad;
             }
-        }  
+        }
     }
 }
 }
 
 void compute_prewitt_dynamic()
 {
+/* 3x3 Prewitt mask for X Dimension. */
+int maskX[3][3];
+maskX[0][0] = +1; maskX[0][1] = 0; maskX[0][2] = -1;
+maskX[1][0] = +1; maskX[1][1] = 0; maskX[1][2] = -1;
+maskX[2][0] = +1; maskX[2][1] = 0; maskX[2][2] = -1;
 
+/* 3x3 Prewitt mask for Y Dimension. */
+int maskY[3][3];
+maskY[0][0] = +1; maskY[0][1] = +1; maskY[0][2] = +1;
+maskY[1][0] =   0; maskY[1][1] =   0; maskY[1][2] =    0;
+maskY[2][0] =  -1; maskY[2][1] =  -1; maskY[2][2] =  -1;
+
+int x = 0;
+int y = 0;
+int chunk_size = image_height/numChunks;
+
+#pragma omp parallel private(x) shared(inputImage, outputImage, image_height, image_width, thread_starts) num_threads(8)
+{
+    #pragma omp for schedule(dynamic, chunk_size) private(y)
+    for(x = 0; x < image_height; ++x ){
+        if (x%chunk_size == 0) {
+            thread_starts[omp_get_thread_num()].push_back(x);
+        }
+        for (y = 0; y < image_width;  ++y ){
+            int grad_x = 0;
+            int grad_y = 0;
+            int grad = 0;
+            /* For handling image boundaries */
+            if( x == 0 || x == (image_height-1) || y == 0 || y == (image_width-1))
+                grad = 0;
+            else{
+                /* Gradient calculation in X Dimension */
+                for(int i = -1; i <= 1; i++ )  {
+                    for(int j = -1; j <= 1; j++ ){
+                        grad_x += (inputImage[x+i][y+j] * maskX[i+1][j+1]);
+                    }
+                }
+                /* Gradient calculation in Y Dimension */
+                for(int i=-1; i<=1; i++)  {
+                    for(int j=-1; j<=1; j++){
+                        grad_y += (inputImage[x+i][y+j] * maskY[i+1][j+1]);
+                    }
+                }
+                /* Gradient magnitude */
+                grad = (int) sqrt( (grad_x * grad_x) + (grad_y * grad_y) );
+            }
+            if (grad < 0) grad = 0;
+            if (grad > 255) grad = 255;
+            #pragma omp critical 
+            {
+                outputImage[x][y] = grad;
+            }
+        }
+    }
+}
 }
 /* **************** Change the function below if you need to ***************** */
 
@@ -166,11 +227,15 @@ int main(int argc, char* argv[])
         double dtime_static = omp_get_wtime();
         compute_prewitt_static();
         dtime_static = omp_get_wtime() - dtime_static;
+        std::cout << dtime_static << std::endl;
     } else {
         double dtime_dyn = omp_get_wtime();
         compute_prewitt_dynamic();
         dtime_dyn = omp_get_wtime() - dtime_dyn;
+        std::cout << dtime_dyn << std::endl;
     }
+
+    
 
     /* ********Start writing output to your file************ */
     std::ofstream ofile(argv[2]);
@@ -188,5 +253,12 @@ int main(int argc, char* argv[])
         std::cout << "ERROR: Could not open output file " << argv[2] << std::endl;
         return 0;
     }
+
+    for (long unsigned i = 0; i < thread_starts.size(); i++) {
+        for (long unsigned j = 0; j < thread_starts[i].size(); j++) {
+            std::cout << i << " with " << thread_starts[i][j] << std::endl;
+    }
+    }
+
     return 0;
 }
